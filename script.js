db907528ff7c5097a9b543ebd917ab6b7094fd8a
Ari Kurniawan <39238418+idearik@users.noreply.github.com>
2024-06-21 15:50:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,28 +6,28 @@ const khodamList = [
     },
     {
         "name": "Naga Putih",
-        "description": "Kamu dilindungi oleh naga putih legendaris yang selalu setia menemanimu. Dengan sayap perkasa dan napas sakti, naga putih ini menjaga keselamatanmu dan memberikan rasa aman dalam setiap perjalanan yang kamu tempuh",
+        "description": "Kamu dilindungi oleh naga putih legendaris yang selalu setia menemanimu. Dengan sayap perkasa dan napas sakti, naga putih ini menjaga keselamatanmu dan memberikan rasa aman dalam setiap perjalanan yang kamu tempuh.",
         "image": "khodam_naga_putih.jpg",
     },
     {
         "name": "Kera Putih",
         "description": "Kamu ditemani oleh kera putih ajaib yang setia menjaga dan membantumu. Dengan kecerdikan dan ketangkasannya, kera putih ini selalu memastikan keselamatanmu dan memberikan kenyamanan dalam setiap petualanganmu.",
-        "image": "khodam_naga_putih.jpg",
+        "image": "khodam_kera_putih.jpg",
     },
     {
         "name": "Macan Putih",
         "description": "Kamu dilindungi oleh macan putih yang gagah dan penuh kharisma. Dengan kekuatannya yang luar biasa dan ketajaman indranya, macan putih ini selalu siap menjaga keselamatanmu dan memberikan rasa percaya diri dalam setiap langkah yang kamu ambil.",
-        "image": "khodam_naga_putih.jpg",
+        "image": "khodam_macan_putih.jpg",
     },
     {
         "name": "Kera Marah",
-        "description": "Kamu ditemani oleh kera merah yang penuh energi dan keberanian. Dengan ketangkasan dan keberaniannya, kera merah ini selalu menjaga keselamatanmu dan memberikan semangat dalam setiap tantangan yang kamu hadapi",
-        "image": "khodam_naga_putih.jpg",
+        "description": "Kamu ditemani oleh kera merah yang penuh energi dan keberanian. Dengan ketangkasan dan keberaniannya, kera merah ini selalu menjaga keselamatanmu dan memberikan semangat dalam setiap tantangan yang kamu hadapi.",
+        "image": "khodam_kera_merah.jpg",
     },
     {
         "name": "Naga Merah",
-        "description": "Kamu dilindungi oleh naga merah yang kuat dan penuh semangat. Dengan kekuatan api dan sayap megahnya, naga merah ini selalu memastikan keselamatanmu dan memberikan perlindungan dalam setiap rintangan yang kamu lalui",
-        "image": "khodam_naga_putih.jpg",
+        "description": "Kamu dilindungi oleh naga merah yang kuat dan penuh semangat. Dengan kekuatan api dan sayap megahnya, naga merah ini selalu memastikan keselamatanmu dan memberikan perlindungan dalam setiap rintangan yang kamu lalui.",
+        "image": "khodam_naga_merah.jpg",
     },
     // Add more khodam objects here
 ];
